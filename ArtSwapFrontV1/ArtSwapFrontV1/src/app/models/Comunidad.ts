f449eabec08dd9fb958e203f3769f4e7f232b629
Kevinski2004 <kevinayquipa5@gmail.com>
2024-11-13 00:40:44 -0500

    update

--- ArtSwapFrontV1/ArtSwapFrontV1/src/app/models/Comunidad.ts
+++
@@ -1,6 +0,0 @@
-export class Comunidad{
-    idComunidad:number=0
-    nombre:string=""
-    descripcion:string=""
-    fechaCreacion: Date= new Date(Date.now())
-}
--- ArtSwapFrontV1/ArtSwapFrontV1/src/app/models/Metodopago.ts
+++
@@ -1,5 +0,0 @@
-export class Metodopago{
-    idMetodoPago:number=0
-    tipo:string=""
-    descripcion:string=""
-}
